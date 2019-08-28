b67c5827dfd72fe6196797dccbc474b79b0c2a2b
CamilaRivera <camila.riverart@gmail.com>
2019-08-28 21:02:29 +0000

    Input Module done

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
-// play.js
 const { connect } = require('./client');
-console.log('Connecting ...');
-/**
- * Setup User Interface 
- * Specifically, so that we can handle user input via stdin
- */
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  handleUserInput();
-  return stdin;
-}
+const { setupInput } = require('./input');
 
-const handleUserInput = function () {
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    }
-  });
-}
+console.log('Connecting ...');
 connect();
 
+setupInput();
