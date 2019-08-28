ffe28cd21190e4f1e3c4ba5819dc8ada63c175c2
CamilaRivera <camila.riverart@gmail.com>
2019-08-28 20:56:14 +0000

    Stdin Setup done

--- play.js
+++ play.js
@@ -1,4 +1,24 @@
 // play.js
 const { connect } = require('./client');
 console.log('Connecting ...');
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  handleUserInput();
+  return stdin;
+}
+
+const handleUserInput = function () {
+  stdin.on('data', (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+  });
+}
 connect();
+
