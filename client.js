3f8012532a1dafd12d2a9a7540ee5e4b9982223f
CamilaRivera <camila.riverart@gmail.com>
2019-08-28 20:27:55 +0000

    Move Commands done

--- client.js
+++ client.js
@@ -16,6 +16,7 @@ const connect = function() {
   conn.on('connect', () => {
     console.log('Successfully connected to game server');
     conn.write('Name: CRR');
+    // conn.write('Move: up'); Hard coded move up
   });
   return conn;
 }
