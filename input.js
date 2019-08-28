a0f728af1d599aff3fae47145718c78b21dca79a
CamilaRivera <camila.riverart@gmail.com>
2019-08-28 21:23:40 +0000

    Stdin Setup and movements print done

--- input.js
+++ input.js
@@ -2,20 +2,36 @@
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
+
+const stdin = process.stdin;
 const setupInput = function () {
-  const stdin = process.stdin;
+  
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
-  handleUserInput();
+  stdin.on('data', handleUserInput);
   return stdin;
 }
 
-const handleUserInput = function () {
-  stdin.on('data', (key) => {
+
+
+const handleUserInput = function (key) {
+ 
     if (key === '\u0003') {
       process.exit();
     }
-  });
+    if (key === 'w') {
+      console.log('up');
+    }
+    if (key === 's') {
+      console.log('down');
+    }
+    if (key === 'd') {
+      console.log('right');
+    }
+    if (key === 'a') {
+      console.log('left');
+    }
 }
+
 
 module.exports = { setupInput: setupInput };
